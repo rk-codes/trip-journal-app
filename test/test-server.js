dd559cc9d69430632ea3c75b0a0444b9e424af65
rk-codes <ping.you.back@gmail.com>
2017-12-20 17:50:22 +0530

    add test

---
+++ test/test-server.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../server.js');
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('Landing Page', function() {
+	it('should return success status', function(){
+		return chai.request(app)
+		.get('/')
+		.then(function(res) {
+			res.should.have.status(200);
+		});
+	});
+
+});
