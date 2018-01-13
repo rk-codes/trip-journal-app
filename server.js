cde01e3f0b8423e9c2775cb2b694aa1496784614
rk-codes <ping.you.back@gmail.com>
2018-01-13 00:30:25 -0800

    remove endpoints

--- server.js
+++ server.js
@@ -3,15 +3,5 @@ const app = express();
 app.use(express.static('public'));
 
 
-
-//Endpoints
-app.post('/signup', function(req, res) {
-
-});
-
-app.post('/login', function(req, res) {
-
-});
-
 app.listen(process.env.PORT || 8080);
 module.exports = app;
