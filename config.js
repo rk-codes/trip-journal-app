c058fc5008f3811eb321f6687ab4bb6a02585ee2
rk-codes <ping.you.back@gmail.com>
2018-01-16 00:35:56 -0800

    add config

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/trip-journal-app';
+exports.PORT = process.env.PORT || 8080;
+
+exports.JWT_SECRET = process.env.JWT_SECRET || 'testing';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
