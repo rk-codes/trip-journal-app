df2bd7da76c28c2d17168f29813ef1b488d5dd09
rk-codes <ping.you.back@gmail.com>
2018-01-16 00:35:17 -0800

    add model and routes for user

---
+++ user/models.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = mongoose.Schema({
+	username: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	firstName: {type: String, default: ''},
+  lastName: {type: String, default: ''},
+  trips: [{
+  	type: mongoose.Schema.Types.ObjectId,
+  	ref: "Trip"
+  }]
+});
+
+UserSchema.methods.serialize = function() {
+  return {
+    username: this.username || '',
+    firstName: this.firstName || '',
+    lastName: this.lastName || '',
+    trips: this.trips.map(trip => trip.serialize()) || []
+  };
+};
+
+UserSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+UserSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 10);
+};
+UserSchema.statics.findByUserName = function(username) {
+	return this.findOne({username: username});
+}
+
+// UserSchema.statics.login = function(username, password) {
+//   const user = findByUserName(username);
+//   if(user && user.validatePassword(password)) {
+//     return user;
+//   } else {
+//     return;
+//   }
+// }
+
+// UserSchema.statics.createWithPassword = function(username, password, firstName = '', lastName = '') {
+
+//   return hashPassword(password). then (function(hashedPassword) {
+//     return User.create({
+//       name: username,
+//       password: hashedPassword,
+//       firstName: firstName,
+//       lastName: lastName
+//     });
+  
+// }
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
+
+//.populate({path: 'trips', populate: {path: 'places'}}) - TODO
