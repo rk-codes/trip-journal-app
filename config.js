ec639d1006b33ae0da1658f40a429ed5fa992abc
rk-codes <ping.you.back@gmail.com>
2018-01-19 01:18:07 -0800

    add test database

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/trip-journal-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-trip-journal-app';
 exports.PORT = process.env.PORT || 8080;
 
 exports.JWT_SECRET = process.env.JWT_SECRET || 'testing';
